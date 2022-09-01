9605166cce834a3beb6244b7cd80037efc540468
Bloublu <bastien.benariac@gmail.com>
2022-09-01 17:42:03 +0200

    ajout taches ok

--- todo_list/app.js
+++ todo_list/app.js
@@ -2,10 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const tasks = [
+    {
+        title: "apprendre Nodejs",
+        done: false,
+    },
+    {
+        title: "faire les courses",
+        done: true,
+    }
+];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('todolist');
+    res.render('todolist', {tasks: tasks});
+});
+
+app.post('/task', (req, res) => {
+    tasks.push({
+        title: req.body.task,
+        done: false,
+    });
+    res.redirect('/');
 });
 
 app.listen(port, (req, res) => {
